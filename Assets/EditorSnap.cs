dd2e80a852bdadc1f60406269077870a1bb4df5c
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2020-11-16 22:01:27 +0200

    5. [ExecuteInEditMode] Attribute

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] float grideSize = 10f;
+
+    void Update()
+    {
+        Vector3 snap;
+        snap.x = Mathf.RoundToInt(transform.position.x / grideSize) * grideSize;    
+        snap.z = Mathf.RoundToInt(transform.position.z / grideSize) * grideSize;
+
+        transform.position = new Vector3(snap.x, 0f, snap.z);
+    }
+}
