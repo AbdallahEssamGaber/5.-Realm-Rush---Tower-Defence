4ce829a49cada22ac86637be037eee531a636b84
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2020-11-20 15:11:51 +0200

    8. Introducing Coroutines

--- Assets/enemy.cs
+++ Assets/enemy.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class enemy : MonoBehaviour
 {
 
-    [SerializeField] List<Block> paths;
+    [SerializeField] List<Waypoint> path;
 
     void Start()
     {
-        foreach(Block path in paths)
-        {
-            print(path);
-        }
+        
+        StartCoroutine(FollowPath());
+        
     }
 
-    void Update()
+    IEnumerator FollowPath()
     {
-       
+        print("Strating patrol....");
+        foreach (Waypoint waypoint in path)
+        {
+            Vector3 palying;
+            palying.x = waypoint.transform.position.x;
+            palying.z = waypoint.transform.position.z;
+            transform.position = new Vector3(palying.x, gameObject.transform.position.y, palying.z);
+            print("Visiting block: " + waypoint.name);
+
+            yield return new WaitForSeconds(1);
+        }
+        print("Ending patrol");
     }
+
 }
