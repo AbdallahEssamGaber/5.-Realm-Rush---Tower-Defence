49a8a7df2e411c7d49b2462206e539823a83979a
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2020-11-29 12:14:30 +0200

    Add comments

--- Assets/PathFinder.cs
+++ Assets/PathFinder.cs
@@ -11,7 +11,7 @@ public class PathFinder : MonoBehaviour
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
 
 
-    [SerializeField] Waypoint cube;
+    [SerializeField] Waypoint cube; //This for the prefab cube to make new one
 
     Vector2Int[] directions = {
         Vector2Int.up, 
@@ -38,7 +38,7 @@ public class PathFinder : MonoBehaviour
     void ExploreNeighbour()
     {
         int timesInLoop = 0;        //Get the time in the foreach so it can decide what side
-        int gridSize = StartCube.GetGrideSize();
+        int gridSize = StartCube.GetGrideSize();    //This for gettign the grid size to multiply by the postition to make grid size
         foreach (Vector2Int direction in directions)
         {
             
@@ -49,11 +49,12 @@ public class PathFinder : MonoBehaviour
             }
             catch
             {
-                Waypoint newCube = Instantiate(cube, new Vector3(exploredCoor.x * gridSize, 0, exploredCoor.y * 10) , Quaternion.identity);
-                newCube.transform.parent = gameObject.transform;
+               
+                Waypoint newCube = Instantiate(cube, new Vector3(exploredCoor.x * gridSize, 0, exploredCoor.y * 10) , Quaternion.identity);     //Make a new cube with right coor
+                newCube.transform.parent = gameObject.transform;    //Put it child to World object
 
-                grid.Add(newCube.GetGridPos(), newCube);
-                newCube.SetColor(Color.yellow);
+                grid.Add(newCube.GetGridPos(), newCube);    //And then add it to the dictionary to color it 
+                newCube.SetColor(Color.yellow);         //Coloring it
 
                 Debug.LogWarning("There is no cube in " + dirNames[timesInLoop] + "Side, I ADDED ONE FOR U :)");
             }
