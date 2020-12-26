97dee9a52814d2404d4b86b46232b1acee2b7c79
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2020-12-26 21:37:48 +0200

    28. Check For Distance
    So here we have done some fun things..we have made the distance to shoot cuz it doesn't make sense that it can fire from big distance also fixing null if the enemy dies

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,39 @@ public class Tower : MonoBehaviour
 {
     [SerializeField] Transform objectToPan;
     [SerializeField] Transform objectEnemy;
+    [SerializeField] float range = 10f;
+    [SerializeField] ParticleSystem shootParticle;
 
     void Update()
     {
-        objectToPan.LookAt(objectEnemy);
+        if(objectEnemy)
+        {
+            objectToPan.LookAt(objectEnemy);
+            FireAtEnemy();
+        }
+        else
+        {
+            shoot(false);
+        }
+        
+    }
+
+    void FireAtEnemy()
+    {
+        float distance = Vector3.Distance(objectEnemy.transform.position, gameObject.transform.position);
+        if(distance <= range)
+        {
+            shoot(true);
+        }
+        else
+        {
+            shoot(false);
+        }
+    }
+
+    void shoot(bool isActive)
+    {
+        var emissionModule = shootParticle.emission;
+        emissionModule.enabled = isActive;
     }
 }
